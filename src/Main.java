df68e280c1ecc55c875c7ccf123589b155e938f8
Nikolay Potapov <potapov4444@gmail.com>
2021-10-23 22:33:33 +0700

    first

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 2000000000;
+        int count = 500000000;
+        int total = price + count;
+        System.out.println(total);
+    }
+}
